7f40b3ff6cf75700a87179698a8f9a00e7ee866e
Dinesh-officiall <officialldinesh@gmail.com>
2024-10-07 18:05:30 -0500

    new coomit

---
+++ index.js
@@ -0,0 +1,17 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar()
+{
+    sidenav.style.left="0"
+}
+
+function closeNavbar()
+{
+    sidenav.style.left="-60%"
+}
+
+
+
+
+
